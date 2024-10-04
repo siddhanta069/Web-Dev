3679f7f21ddfc73df2796fdba1561ff7935c7892
siddhanta sahoo <siddhanta027z@gmail.com>
2024-10-04 18:28:25 +0530

    added more function

--- mongoDB/db.js
+++ mongoDB/db.js
@@ -4,7 +4,7 @@ const ObjectId = mongoose.ObjectId
 
 
 const User = new Schema ({
-    email:String,
+    email: {type:String, unique:true},
     password:String,
     name:String
 })
--- mongoDB/index.js
+++ mongoDB/index.js
@@ -3,26 +3,60 @@ const {UserModel, TodoModel} = require("./db")
 const jwt = require("jsonwebtoken")
 const JWT_SECRET = "SydenHarbourBridge"
 const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const { z } = require("zod")
 
 const app = express()
 app.use(express.json())
-mongoose.connect("mongodb+srv://siddhanta027z:inqSvS92F7lenQ9T@cluster0.ga5zu.mongodb.net/todo-siddh-app")
+mongoose.connect("")
 
 app.post("/signup",async function(req, res) {
-    const email = req.body.email
-    const password = req.body.password
-    const name = req.body.name
-
-    await UserModel.create({
-        email: email,
-        password: password,
-        name: name
+    // input validation using zod
+    const userSchema = z.object({
+        email: z.string().min(3).max(20).email(),
+        password: z.string().min(8).max(20),
+        name: z.string().max(20)
     })
+    // const parsedDataWithSuccess = userSchema.parse(req.body)  //this return only data if any error it will stop there
+    const parsedDataWithSuccess = userSchema.safeParse(req.body) //this return the object {success: true | false, data: {}, error: []} 
 
-    res.json({
-        message: "You are Signed Up"
-    })
-    
+    if(!parsedDataWithSuccess.success) {
+        res.json({
+            message: "Incorrect format",
+            error: parsedDataWithSuccess.error
+        })
+
+        return 
+    }
+    // error handling
+    try {
+            
+        const email = req.body.email
+        const password = req.body.password
+        const name = req.body.name
+
+        // hashing password
+        const hashpassword = await bcrypt.hash(password, 5)
+
+        await UserModel.create({
+            email: email,
+            password: hashpassword,
+            name: name
+        })
+
+        res.json({
+            message: "You are Signed Up"
+        })
+    } catch(e) {
+        res.json({
+            message: "User is Already exists"
+        })
+    }
+
+    // another way to through error
+    // throw new Error("user is Already exists")
+
+
 })
 
 
@@ -31,12 +65,21 @@ app.post("/signin",async function(req, res) {
     const password = req.body.password
 
     const user = await UserModel.findOne ({
-        email: email,
-        password: password
+        email: email
     })
-    console.log(user)
 
-    if(user) {
+    if(!user) {
+        res.json({
+            message: "User does not exist."
+        })
+
+        return 
+    }
+
+    const matchUser = await bcrypt.compare(password, user.password)
+    
+
+    if(matchUser) {
         const token = jwt.sign({
             id: user._id.toString()
         },JWT_SECRET)
